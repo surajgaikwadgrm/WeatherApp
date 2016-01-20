7287aabde970b998e17d396fce5ce10f3b5c5d24
Suraj Gaikwad <suraj@Orphan-Mac-mini.local>
2016-01-20 15:06:58 +0530

    Add Android project

---
+++ app/src/main/java/com/raremile/suraj/weatherapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.raremile.suraj.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showListOfCities();
+    }
+
+    private void showListOfCities(){
+        
+    }
+}
